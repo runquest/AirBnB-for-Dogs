2ea64e02dab2cd183718e4b4afa87df90b8a5221
Eli Siegel <elicsiegel@gmail.com>
2015-03-26 20:34:33 +0000

    added initial actions to site

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,18 @@
 get '/' do
   erb :index
 end
+
+get '/hosts' do 
+  @hosts = Host.all 
+  erb :hosts
+end
+
+get '/hosts/:id' do 
+  @host = Host.find params[:id]
+  erb :show_host
+end
+
+get '/dogs/:id' do 
+  @dog = Dog.find params[:id]
+  erb :show_dog
+end
