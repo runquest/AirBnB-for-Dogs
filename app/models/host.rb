b463ca7d883e927376fee5c6eb7cb2049a5e447a
Eli Siegel <elicsiegel@gmail.com>
2015-03-27 23:49:19 +0000

    add specific host information

--- app/models/host.rb
+++ app/models/host.rb
@@ -5,4 +5,15 @@ class Host < ActiveRecord::Base
   validates :email, uniqueness: true, format: { with: /\A([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)\z/ }
   has_many :bookings
   scope :profile_pic, ->(booking) { where(id: booking.host_id).first.profile_picture }
+
+  def average_rating
+    total_feedback = 0
+
+    bookings.each do |booking|
+      total_feedback += booking.dog_feedback
+    end
+    total_feedback/bookings.length
+  end
+
+
 end
