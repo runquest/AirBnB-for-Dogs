942c643d205f6f30f4bb85628112047e8ecf5372
amarbirgill <amarbir_151@hotmail.com>
2015-03-28 03:41:14 +0000

    simple google map working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150327200717) do
 
   create_table "bookings", force: true do |t|
