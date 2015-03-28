4d9166187e32cc43818e55b50e8b6c08aef02aad
Eli Siegel <elicsiegel@gmail.com>
2015-03-28 20:43:47 +0000

    add basic signup page

--- app/actions.rb
+++ app/actions.rb
@@ -15,6 +15,22 @@ get '/hosts' do
   erb :hosts
 end
 
+get '/new' do 
+  erb :new
+end
+
+post '/users' do 
+  @user = Host.new(
+    name: params[:name],
+    email: params[:email],
+    address: params[:address],
+    city: params[:city],
+    country: params[:country]
+    )
+  @user.save
+  redirect '/'
+end
+
 get '/hosts/:id' do 
   @host = Host.find params[:id]
   erb :show_host
